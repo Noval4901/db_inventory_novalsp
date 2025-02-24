4250ff71554835485a6f816ff78cb5a909d5f054
NightHowlers <novalmarhas@gmail.com>
2025-02-24 13:47:13 +0700

    Add files via upload

---
+++ inventory_novalsp_xi1/Config/Koneksi.php
@@ -0,0 +1,13 @@
+<?php 
+
+//variable
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database_name = 'db_inventory_novalsp';
+
+$conn = mysqli_connect($hostname, $username, $password, $database_name);
+
+if(!$conn){
+    die('Koneksi Gagal'.mysqli_connect_error());
+}?>
